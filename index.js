86714d5d9992e99544fd34febfd2bbbc1c8ff348
jozu2 <it.joshuamelendres@gmail.com>
2023-02-03 14:58:08 +0800

    analog clock done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-alert('hi')
+const hourHand = document.querySelector("[data-hour-hand]");
+const minuteHand = document.querySelector("[data-minute-hand]");
+const secondHand = document.querySelector("[data-second-hand]");
+
+setInterval(setClock, 1000);
+
+function setClock() {
+  const currentTime = new Date();
+
+  const secondRatio = currentTime.getSeconds() / 60;
+  const minutesRatio = (secondRatio + currentTime.getMinutes()) / 60;
+  const hoursRatio = (minutesRatio + currentTime.getHours()) / 12;
+  setRotation(hourHand, hoursRatio);
+  setRotation(minuteHand, minutesRatio);
+  setRotation(secondHand, secondRatio);
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty("--rotation", rotationRatio * 360);
+}
+
+setClock();
